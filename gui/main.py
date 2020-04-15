6e4cc939a0130b69a177ec85b513187cc858fc72
Ignacio <nahnero@gmail.com>
2020-04-15 23:12:31 +0200

    .

--- gui/main.py
+++ gui/main.py
@@ -8,30 +8,25 @@ class Ui(QtWidgets.QMainWindow):
 
         self.button = self.findChild (QtWidgets.QPushButton, 'pushButton')
 
-        self.res1 = self.findChild (QtWidgets.QLabel, 'res1')
-        self.res2 = self.findChild (QtWidgets.QLabel, 'res2')
-        self.res3 = self.findChild (QtWidgets.QLabel, 'res3')
-        self.titl = self.findChild (QtWidgets.QLabel, 'label_4')
+        self.res = []
+        for i in range (3):
+            self.res.append (self.findChild (QtWidgets.QLabel, 'res'+str(i+1)))
 
-        self.var1 = self.findChild (QtWidgets.QLineEdit, 'var1LE')
-        self.var2 = self.findChild (QtWidgets.QLineEdit, 'var2LE')
-        self.var3 = self.findChild (QtWidgets.QLineEdit, 'var3LE')
+        self.var = []
+        for i in range (3):
+            self.var.append (self.findChild (QtWidgets.QLineEdit, 'var'+str(i+1)+'LE'))
 
         self.button.clicked.connect (self.printButtonPressed)
         self.show()
 
     def printButtonPressed (self):
-        self.res1.setText (self.var1.text ())
-        self.res2.setText (self.var2.text ())
-        self.res3.setText (self.var3.text ())
-        self.titl.setText ("you gon' die boy")
-        self.res1.adjustSize()
-        self.res2.adjustSize()
-        self.res3.adjustSize()
-        self.titl.adjustSize ()
-        print (self.var1.text ())
-        print (self.var2.text ())
-        print (self.var3.text ())
+
+        [res.setText (var.text ()) for var, res in zip (self.var, self.res)]
+        [res.adjustSize () for res in self.res]
+        [print (var.text ()) for var in self.var]
+
+        [var.clear () for var in self.var]
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
